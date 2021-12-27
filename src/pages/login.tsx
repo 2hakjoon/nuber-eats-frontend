784159d42b4cd936f3d2e5f656b9f16bc5b2edcc
LeeHakJoon <2hakjoon@gmail.com>
2021-12-27 18:05:45 +0900

    setup is done realy

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Login = () => {
 	return (
-		<div className="h-screen flex items-center justify-center bg-red-800">
+		<div className="h-screen flex items-center justify-center bg-blue-800">
 			<div className="bg-white px-10 py-5">
 				<h3 className="font-bold text-lg text-blue-800">Log In</h3>
 			</div>
