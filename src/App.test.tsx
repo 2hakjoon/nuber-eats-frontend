c7e56372f44db35bafe162fc8a6b91eab9a0e954
LeeHakJoon <2hakjoon@gmail.com>
2022-01-02 19:58:15 +0900

    add test for App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import { isLoggedInVar } from './apollo';
 
-test('renders learn react link', () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+const LOGGED_OUT_TEXT = '로그아웃 컴포넌트 텍스트';
+const LOGGED_IN_TEXT = '로그인 컴포넌트 텍스트';
+
+jest.mock('./routers/logged-out-router', () => {
+	return function () {
+		return <div>{LOGGED_OUT_TEXT}</div>;
+	};
+});
+
+jest.mock('./routers/logged-in-router', () => {
+	return function () {
+		return <div>{LOGGED_IN_TEXT}</div>;
+	};
+});
+
+describe('<App />', () => {
+	it('renders LoggedOutRouter', () => {
+		const { getByText } = render(<App />);
+		getByText(LOGGED_OUT_TEXT);
+	});
+	it('renders LoggedInRouter', async () => {
+		const { getByText } = render(<App />);
+		await waitFor(() => {
+			isLoggedInVar(true);
+		});
+		getByText(LOGGED_IN_TEXT);
+	});
 });
